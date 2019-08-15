9c9d8c0ee0793bd0ba824810bc57dd238852f32f
Mike Lewis <mlewis@gmail.com>
2019-08-15 14:48:19 -0400

    Adding button test

--- __tests__/Button.test.tsx
+++ __tests__/Button.test.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import Button from '../components/form/Button';
 import { mountWithTheme } from '../utils/test-utils';
 
-
-
 describe('Button', () => {
   it('renders without error', () => {
-    expect(mountWithTheme(<Button type="button" onClick={() => {}}>TEst</Button>).find('button').length === 1).toBe(true);
+    expect(mountWithTheme(<Button type="button" onClick={() => {}}>Test</Button>).find('button').length === 1).toBe(true);
+  });
+  it('calls the onClick callback', () => {
+    const mockCallback = jest.fn();
+    const button = mountWithTheme(<Button type="button" onClick={mockCallback}>Test</Button>);
+    button.find('button').simulate('click');
+    expect(mockCallback.mock.calls.length).toEqual(1);
   });
 });
