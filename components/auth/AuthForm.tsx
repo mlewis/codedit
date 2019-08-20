583b0748c42b2df7c3427d63b376ad75de54a49b
Mike Lewis <mlewis@gmail.com>
2019-08-20 14:59:08 -0400

    Abstract out auth form

---
+++ components/auth/AuthForm.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import { Formik, FormikProps, FormikValues } from 'formik';
+import * as Yup from 'yup';
+
+import Button from '../form/Button';
+import { FormField } from '../../interfaces';
+import { getFormFieldDetails, getErrorDetailsForField } from '../../utils/form-utils';
+import Input from '../form/Input';
+import TransparentButton from '../common/TransparentButton';
+
+type Props = {
+  submitCallback: (values: FormikValues) => void,
+  formFields: Array<FormField>,
+}
+
+const AuthForm: React.FC<Props> = ({ submitCallback, formFields }): React.ReactElement => {
+  const router = useRouter();
+  const [success, setSuccess] = useState(false);
+  const { shape, initialValues } = getFormFieldDetails(formFields);
+  type FormValues = Yup.InferType<typeof shape>;
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={async (values, { setSubmitting}): Promise<void> => {
+        await submitCallback(values);
+        setSubmitting(false);
+        setSuccess(true);
+      }}
+      validationSchema={shape}
+    >
+      {(props: FormikProps<FormValues>): React.ReactElement => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleSubmit
+        } = props;
+        return (
+          <FormWrapper>
+            {success ?
+              <div>
+                You are now logged in.
+                <p>
+                  <TransparentButton type="button" onClick={(): void => { router.back(); }}>Go back</TransparentButton>
+                </p>
+              </div>
+            :
+            <form onSubmit={handleSubmit}>
+              {formFields.map((field: FormField) => (
+                <InputWrapper key={`form-field-${field.name}`}>
+                  {getErrorDetailsForField(field.name, errors, touched)}
+                  <Input
+                    name={field.name}
+                    type={field.type}
+                    value={values[field.name]}
+                    onChange={handleChange}
+                    placeholder={field.placeholder}
+                  />
+                </InputWrapper>
+              ))}
+              <Button disabled={isSubmitting} onClick={() => { handleSubmit(); }} type="submit">Login</Button>
+            </form>
+          }
+          </FormWrapper>
+        )
+      }}
+    </Formik>
+  );
+}
+
+const InputWrapper = styled.div`
+  margin-bottom: 20px; 
+`;
+
+const FormWrapper = styled.div`
+  margin-top: 20px;
+`;
+
+export default AuthForm;
--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,17 +1,11 @@
-import React, { useState, ReactElement } from 'react';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import { Formik, FormikProps } from 'formik';
-import * as Yup from 'yup';
+import React from 'react';
+import { FormikValues } from 'formik';
 
-import Button from '../form/Button';
+import AuthForm from './AuthForm';
 import { FormField } from '../../interfaces';
-import { getFormFieldDetails, getErrorDetailsForField } from '../../utils/form-utils';
 import { H2 } from '../common/Headers';
-import Input from '../form/Input';
 import { makePostRequest } from '../../utils/API';
 import { saveItem } from '../../utils/storage';
-import TransparentButton from '../common/TransparentButton';
 
 const formFields: Array<FormField> = [
   {
@@ -30,80 +24,24 @@ const formFields: Array<FormField> = [
   },
 ];
 
-const { shape, initialValues } = getFormFieldDetails(formFields);
-type FormValues = Yup.InferType<typeof shape>;
-
-const Login: React.FC = (): ReactElement => {
-  const router = useRouter();
-  const [loginSuccess, setLoginSuccess] = useState(false);
-
-  return (
-    <div>
-      <H2>Login</H2>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={async (values, { setSubmitting}): Promise<void> => {
-          const response = await makePostRequest('login', {
-            email: values.email,
-            password: values.password,
-          });
-          if (response && response.token) {
-            saveItem('token', response.token);
-            setLoginSuccess(true);
-          }
-          setSubmitting(false);
-        }}
-        validationSchema={shape}
-      >
-        {(props: FormikProps<FormValues>): React.ReactElement => {
-          const {
-            values,
-            touched,
-            errors,
-            isSubmitting,
-            handleChange,
-            handleSubmit
-          } = props;
-          return (
-            <FormWrapper>
-              {loginSuccess ?
-                <div>
-                  You are now logged in.
-                  <p>
-                    <TransparentButton type="button" onClick={(): void => { router.back(); }}>Go back</TransparentButton>
-                  </p>
-                </div>
-              :
-              <form onSubmit={handleSubmit}>
-                {formFields.map((field: FormField) => (
-                  <InputWrapper key={`form-field-${field.name}`}>
-                    {getErrorDetailsForField(field.name, errors, touched)}
-                    <Input
-                      name={field.name}
-                      type={field.type}
-                      value={values[field.name]}
-                      onChange={handleChange}
-                      placeholder={field.placeholder}
-                    />
-                  </InputWrapper>
-                ))}
-                <Button disabled={isSubmitting} onClick={() => { handleSubmit(); }} type="submit">Login</Button>
-              </form>
-            }
-            </FormWrapper>
-          )
-        }}
-      </Formik>
-    </div>
-  )
+const submitLogin = async (values: FormikValues) => {
+  const response = await makePostRequest('login', {
+    email: values.email,
+    password: values.password,
+  });
+  if (response && response.token) {
+    saveItem('token', response.token);
+  }
 }
 
-const InputWrapper = styled.div`
-  margin-bottom: 20px; 
-`;
-
-const FormWrapper = styled.div`
-  margin-top: 20px;
-`;
+const Login: React.FC = (): React.ReactElement => (
+  <div>
+    <H2>Login</H2>
+    <AuthForm
+      submitCallback={submitLogin}
+      formFields={formFields}
+    />
+  </div>
+);
 
 export default Login;
